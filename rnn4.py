be3eb50278c14d0522b43b798fa69db18a92066e
SokoSasha <asokolova19@gmail.com>
2024-09-02 23:21:51 +0300

    We are going somethere. Probably.

--- rnn4.py
+++ rnn4.py
@@ -1,146 +1,92 @@
-import numpy as np
-import pandas as pd
-from gensim.models import Word2Vec
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report
 from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.layers import LSTM, Dense, Embedding, InputLayer
+from tensorflow.keras.layers import Bidirectional, LSTM, Dense, Embedding, InputLayer
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
 
-batch_size = 32
+from text_processing import *
 
-
-# Загрузка данных
-def load_data(positive_file, negative_file):
-    with open(positive_file, 'r', encoding='utf-8') as f:
-        positive_reviews = [line.strip() for line in f.readlines()]
-
-    with open(negative_file, 'r', encoding='utf-8') as f:
-        negative_reviews = [line.strip() for line in f.readlines()]
-
-    positive_labels = [1] * len(positive_reviews)
-    negative_labels = [0] * len(negative_reviews)
-
-    all_reviews = positive_reviews + negative_reviews
-    all_labels = positive_labels + negative_labels
-
-    return all_reviews, all_labels
+BATCH_SIZE = 32
+NUM_EPOCHS = 3
 
 
-# Предобработка текста
-def preprocess_text(reviews):
-    processed_reviews = [review.lower().strip() for review in reviews]
-    return processed_reviews
+def show_data(y_pred_probs, y_test, model_desc):
+    y_pred = (y_pred_probs > 0.5).astype(int)
+
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    conf_matrix_normalized = conf_matrix.astype('float') / conf_matrix.sum()
+
+    plt.figure(constrained_layout=True)
+    sns.heatmap(conf_matrix_normalized, annot=True, fmt='.2f', cmap='YlGn', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Confusion Matrix')
+    plt.suptitle(model_desc)
+    plt.show()
+
+    # Отчет по классификации
+    print(classification_report(y_test, y_pred, target_names=['Negative', 'Positive']))
+    print('---------------------------------------------------------------------------------------')
+    print()
 
 
-# Векторизация текста с использованием Word2Vec
-def train_word2vec_model(reviews, vector_size=300, window=5, min_count=1):
-    tokenized_reviews = [review.split() for review in reviews]
-    word2vec_model = Word2Vec(sentences=tokenized_reviews, vector_size=vector_size, window=window, min_count=min_count, workers=4)
-    return word2vec_model
+def get_model_desc(model):
+    model_desc = ""
+    for layer in model.layers:
+        if isinstance(layer, Bidirectional):
+            model_desc += f"{layer.name}({layer.forward_layer.name}) [{layer.input.shape}]\n"
+        elif isinstance(layer, Dense):
+            model_desc += f"{layer.name}({layer.activation.__name__}) [{layer.input.shape}]\n"
+        else:
+            model_desc += f"{layer.name} [{layer.input.shape}]\n"
 
-
-# Преобразование текста в последовательности
-def text_to_sequences(reviews, word2vec_model, max_sequence_length):
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(reviews)
-
-    sequences = tokenizer.texts_to_sequences(reviews)
-    word_index = tokenizer.word_index
-
-    embedding_matrix = np.zeros((len(word_index) + 1, word2vec_model.vector_size))
-    for word, i in word_index.items():
-        if word in word2vec_model.wv:
-            embedding_matrix[i] = word2vec_model.wv[word]
-
-    sequences_padded = pad_sequences(sequences, maxlen=max_sequence_length)
-
-    return sequences_padded, embedding_matrix, tokenizer
+    print(f"[LOG]\n{model_desc}")
+    return model_desc
 
 
 # Создание модели LSTM
-def create_lstm_model(embedding_matrix, max_sequence_length, batch_size):
+def create_lstm_model(embedding_matrix, max_sequence_length, batch_size, activation_f):
     vocab_size, embedding_dim = embedding_matrix.shape
     model = Sequential()
 
+    # Define input data shape
     model.add(InputLayer(batch_input_shape=(batch_size, max_sequence_length,)))
+    #
     model.add(Embedding(input_dim=vocab_size, output_dim=embedding_dim, trainable=False))
-    model.add(LSTM(100, stateful=True, return_sequences=True, dropout=0.2, recurrent_dropout=0.2, unroll=True))
-    model.add(LSTM(50, stateful=True, return_sequences=True, dropout=0.2, recurrent_dropout=0.2, unroll=True))
+    model.add(Bidirectional(LSTM(32, stateful=True, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)))
+    model.add(Bidirectional(LSTM(32, stateful=True, return_sequences=False, dropout=0.2, recurrent_dropout=0.2)))
 
     # Добавление слоя Dense
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1, activation=activation_f))
 
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
     return model
 
 
-def load_and_preprocess_test_data(filepath, tokenizer, max_sequence_length, batch_size):
-    # Загрузка тестовых данных
-    test_data = pd.read_csv(filepath)
+def main():
+    positive_file = "TrainingDataPositive.txt"
+    negative_file = "TrainingDataNegative.txt"
+    test_filepath = 'TestReviews.csv'
 
-    # Разделение на признаки и метки
-    X_test = test_data['review'].values
-    y_test = test_data['class'].values
+    X_train, y_train, X_val, y_val, X_test, y_test, embedding_matrix, max_sequence_length = get_data(positive_file, negative_file, test_filepath, BATCH_SIZE)
 
-    # Преобразование текстов в последовательности
-    X_test = tokenizer.texts_to_sequences(X_test)
-    X_test = pad_sequences(X_test, maxlen=max_sequence_length)
+    for activ in ['sigmoid', 'softplus', 'softsign', 'tanh']:
+        # Создание и обучение модели LSTM
+        model = create_lstm_model(embedding_matrix, max_sequence_length, BATCH_SIZE, activ)
 
-    # Убедитесь, что размер тестовых данных кратен размеру батча
-    test_size = (X_test.shape[0] // batch_size) * batch_size
-    X_test = X_test[:test_size]
-    y_test = y_test[:test_size]
+        model_desc = get_model_desc(model)
 
-    return X_test, y_test
+        checkpoint = ModelCheckpoint('best_model.keras', save_best_only=True, monitor='val_loss', mode='min')
+        history = model.fit(X_train, y_train, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, validation_data=(X_val, y_val), shuffle=False, callbacks=[checkpoint])
+
+        # Оценка модели
+        loss, accuracy = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
+        print(f"Test Accuracy: {accuracy * 100:.2f}")
+
+        y_pred_probs = model.predict(X_test, batch_size=BATCH_SIZE)
+        show_data(y_pred_probs, y_test, model_desc)
 
 
-positive_file = "TrainingDataPositive.txt"
-negative_file = "TrainingDataNegative.txt"
-
-# Загрузка данных
-reviews, labels = load_data(positive_file, negative_file)
-
-# Предобработка текста
-processed_reviews = preprocess_text(reviews)
-
-# Загрузка модели Word2Vec
-word2vec_model = Word2Vec.load('word2vec_model.bin')
-
-# Преобразование текста в последовательности
-max_sequence_length = 100
-X, embedding_matrix, tokenizer = text_to_sequences(processed_reviews, word2vec_model, max_sequence_length)
-
-# Преобразование меток
-y = np.array(labels)
-
-# Разделение данных на обучающую и тестовую выборки
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Убедитесь, что размеры данных кратны размеру батча
-train_size = (X_train.shape[0] // batch_size) * batch_size
-val_size = (X_val.shape[0] // batch_size) * batch_size
-
-X_train = X_train[:train_size]
-y_train = y_train[:train_size]
-
-X_val = X_val[:val_size]
-y_val = y_val[:val_size]
-
-# Загрузка и подготовка тестовых данных
-test_filepath = 'TestReviews.csv'
-X_test, y_test = load_and_preprocess_test_data(test_filepath, tokenizer, max_sequence_length, batch_size)
-
-# Создание и обучение модели LSTM
-model = create_lstm_model(embedding_matrix, max_sequence_length, batch_size)
-
-# Определение контрольной точки для сохранения лучшей модели
-checkpoint = ModelCheckpoint('best_model.keras', save_best_only=True, monitor='val_loss', mode='min')
-
-# Обучение модели
-history = model.fit(X_train, y_train, epochs=5, batch_size=batch_size, validation_data=(X_val, y_val), shuffle=False, callbacks=[checkpoint])
-
-# Оценка модели
-loss, accuracy = model.evaluate(X_test, y_test, batch_size=batch_size)
-print(f"Test Accuracy: {accuracy}")
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from gensim.models import Word2Vec
-from tensorflow.keras.layers import LSTM
+from keras.src.layers import Bidirectional
+from tensorflow.keras.layers import LSTM, Bidirectional
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -46,7 +47,7 @@ def process_review(review, model, tokenizer, word2vec_model, max_sequence_length
 
 def reset_lstm(model):
     for layer in model.layers:
-        if isinstance(layer, LSTM):
+        if isinstance(layer, LSTM) or isinstance(layer, Bidirectional):
             layer.reset_states()
 
 
@@ -58,7 +59,10 @@ def main():
     # Список отзывов для обработки
     reviews = [
         "The location is in a strip mall, but this place is a diamond in the rough.  The food was some of the best italian food I have had anywhere.  I live in Kansas City, which has a pretty large and long standing italian populaiton.  The food is a good as any of the old italian places in KC.  Everyone in my group was amazed at the quality of the food for the price.  I will keep this one on my list of places to come back to when I am in AZ again.",
-        "After being admitted to this facility my elderly friend received a few days treatment and then was constantly badgered to sign into a hospice facility called Solari. She was told by TLC that she could stay there up to 100 days. Then suddenly, after less than 2 weeks she was told she had to leave...period, no notice. Once again the Solari rep showed up only this time she stated that she could only stay 2 days in Solari. Moved her the following day. TLC on site social worker was usually no where to be found and never returned her calls or the many pages the receptionist made for her stating ""I know she's here, I don't know why she's not responding"".Avoid this place at any costs!"
+        "Fantastic spot for an even or a quite cocktail.  They were swell to host the Yelp crew with a great drink menu and super attentive staff.I'd certainly recommend anything with the purred fruit in it (apple, any of them really)!",
+        "Love this place. Stiff martinis and cocktails, cheap drinks, good service, nice atmosphere to chill in the upstairs lounge and hang up with your friends. Classy crowd and much more mature, older, and professional crowd. There aren't any of those college frat boys or belligerent drunks there, which is so great. Very nice place to wind down after a long day.",
+        "Never going back!!This was my second time here. Wasn't that impressed the first time, but thought I'll give it another look, more because one of the guys in our team really wanted to go here. Food: Just ok, over-priced and very small portions. Came out hungry. Deserts are quite niceService: I think I would have given this place 2 12 stars was it not for the bad service. Very rude waiter, I am not going to go into details of what happenned at our table but suffice to say I left the place pissed! Never going back and nor is the guy in our team who really wanted to go in the first place.I think I hate a rude (and I mean rude not bad) service more that bad food. Bad food just leaves me with no feelings but a rude server leaves me pissed!"
+        # "After being admitted to this facility my elderly friend received a few days treatment and then was constantly badgered to sign into a hospice facility called Solari. She was told by TLC that she could stay there up to 100 days. Then suddenly, after less than 2 weeks she was told she had to leave...period, no notice. Once again the Solari rep showed up only this time she stated that she could only stay 2 days in Solari. Moved her the following day. TLC on site social worker was usually no where to be found and never returned her calls or the many pages the receptionist made for her stating ""I know she's here, I don't know why she's not responding"".Avoid this place at any costs!",
     ]
 
     for review in reviews:
@@ -66,9 +70,13 @@ def main():
         review = review.split('.')
         for sentence in review:
             confidence = process_review(sentence, model, tokenizer, word2vec_model, max_sequence_length)
-            print(f"{confidence * 100:.2f}% negative")
+            if confidence > 0.5:
+                print(f"{confidence * 100:.2f}% positive")
+            else:
+                print(f"{confidence * 100:.2f}% negative")
 
-    reset_lstm(model)
+        print()
+        reset_lstm(model)
 
 
 if __name__ == '__main__':
